22a3555e6fcf92bedfa5964ee3d4ea98f4fdcca8
Ash Todd <ashztodd@gmail.com>
2019-10-04 17:56:25 -0700

    at last, gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname),
+        publicPath: '/vivari/',
+        path: path.resolve(__dirname, 'build'),
         filename: 'main.js',
     },
     module: {
