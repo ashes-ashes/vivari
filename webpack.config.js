5aa33a585136654f50244a5c8837b7e4840db473
Ash Todd <ashztodd@gmail.com>
2019-10-07 10:37:44 -0700

    Fixed path error

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        publicPath: '/vivari/',
+        // publicPath: '/vivari/',
         path: path.resolve(__dirname, 'build'),
         filename: 'main.js',
     },
